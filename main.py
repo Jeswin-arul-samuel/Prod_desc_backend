2ec8103fe83442934136b26a0c8bad76c7155f7e
Jeswin <56197634+Jeswin-arul-samuel@users.noreply.github.com>
2025-04-10 10:14:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if not os.path.exists(static_folder):
 # Allow frontend to talk to the backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://prod-desc-frontend.vercel.app/"],
+    allow_origins=["https://prod-desc-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
