078b479ea08ebe6c6b48a99185a8ed1d4c84bc11
Jeswin <56197634+Jeswin-arul-samuel@users.noreply.github.com>
2025-04-10 09:01:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-
+import os
 from routes import upload, generate, health
 
-app = FastAPI(title="Product Metadata Generator")
+app = FastAPI(title="Product Description Generator")
+
+static_folder = "static"
+if not os.path.exists(static_folder):
+    os.makedirs(static_folder)
+    print(f"Created {static_folder} folder.")
 
 # Allow frontend to talk to the backend
 app.add_middleware(
